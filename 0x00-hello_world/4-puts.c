92be22895a75a26d687d03e6e86ca079d6717613
Hiwot <hiwotmolalign2@gmail.com>
2022-09-08 21:22:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
- #include <stdio.h>
-int main(void){ puts("\"Programming is like building a multilingual puzzle");
-       	return (0);}
+#include <stdio.h>
+/**
+ * main function
+ * return 0 if success
+ */
+int main(void)
+{
+	puts("\"programming is like building multilingual puzzel");
+	return (0);
+}

e1c122ddf3129f6969932d33659c93356cde330f
Hiwot <hiwotmolalign2@gmail.com>
2022-09-14 00:08:10 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - to check if c is a letter
+ * @c: a variable for argument
+ * Return: 1 if c is a letter, lowercase or uppercase
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

f80a7f640ac7e07937575cbf42e0eb6e4727b1b3
Hiwot <hiwotmolalign2@gmail.com>
2022-09-14 03:46:53 -0700

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,21 +6,44 @@
  * Return: Always 0.
  */
 
-void print_times_table(int n)
+void print_times_table(int)
 {
-	print_times_table(3);
+	int i, j, k;
 
-	    _putchar('\n');
-
-	        print_times_table(5);
-
-		    _putchar('\n');
-
-		        print_times_table(98);
-
-			    _putchar('\n');
-
-			        print_times_table(12);  
-
-				    return (0);
+	if (!(n > 15 || n< 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n j++)
+			{
+				k = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (k < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100 && j != 0)
+				{
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+					_putchar((k % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }

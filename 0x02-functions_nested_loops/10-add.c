5e1c581dd584b220e25e02e634733c47d7f1b31e
Hiwot <hiwotmolalign2@gmail.com>
2022-09-14 01:57:35 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ * @num1: first integer
+ * @num2: second int
+ * Return: result
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

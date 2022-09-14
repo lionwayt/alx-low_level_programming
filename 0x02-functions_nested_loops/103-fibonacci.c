b875a08523cdd41a87b3a0f555e300dbccb5e26b
Hiwot <hiwotmolalign2@gmail.com>
2022-09-14 06:06:24 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,41 +7,20 @@
 
 int main(void)
 {
-	int count;
-	unsigned long i, j, k;
-	unsigned long a, b, c, carry;
+	unsigned long count, a, b, c, sum;
 
-	count = 0;
-	i = 0;
-	j = 1;
-		for (count = 1; count <= 91; count++)
-		{
-			k = i + j;
-			i = j;
-			j = k;
-			printf("%lu, ", k);
-		}
-	a = i % 1000;
-	i = i / 1000;
-	b = j % 1000;
-	j = j / 1000;
-	while (count <= 98)
+	a = sum = 0;
+	b = 1;
+	for (count = 0; count < 50; count ++)
 	{
-		carry = (a + b) / 1000;
-		c = (a + b) - carry * 1000;
-		k = (i + j) + carry;
+		c = a + b;
 		a = b;
 		b = c;
-		i = j;
-		j = k;
-		if (c >= 100)
-			printf("%lu%lu", k, c);
-		else
-			printf("%lu0%lu", k, c);
-		if (count != 98)
-			printf(", ");
-		count++;
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sum += c;
+		}
 	}
-	putchar('\n');
+	printf("%lu\n", sum);
 	return (0);
 }

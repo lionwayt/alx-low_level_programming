cbc1d4d5474c1a68113f3bca15ae6f33f32ce07c
Hiwot <hiwotmolalign2@gmail.com>
2022-09-14 03:08:20 -0700

    The World looks like a multiplication-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: a variable
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+	print_times_table(3);
+
+	    _putchar('\n');
+
+	        print_times_table(5);
+
+		    _putchar('\n');
+
+		        print_times_table(98);
+
+			    _putchar('\n');
+
+			        print_times_table(12);  
+
+				    return (0);
+}

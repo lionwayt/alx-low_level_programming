b0660d7a5208952834f747f6d7c99025c96d2358
Hiwot <hiwotmolalign2@gmail.com>
2022-09-13 23:01:52 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
@@ -12,7 +13,9 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
 void print_times_table(int n);
 void num(int r, int n);
 
 #endif
+

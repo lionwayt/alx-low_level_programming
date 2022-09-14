efdb31df1a2e8dc16a791c6681f30204ece1e9b3
Hiwot <hiwotmolalign2@gmail.com>
2022-09-14 07:48:55 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- *  Return: 0
+ * Return: 0
  */
 
 int main(void)
@@ -13,10 +13,9 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)*/
-	/*{ */
-        /*putchar(i);*/
-	/*}*/
+	while (i < 10) { 
+        putchar(i);
+	}
 
 	printf("Infinite loop avoided! \\o/\n");
 
